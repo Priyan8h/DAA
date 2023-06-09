0ea83bbe298a6eaa3dbf8d0db4576646533c7047
Priyansh Garg <priyanshgarg.pg@gmail.com>
2023-06-09 22:54:33 +0530

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string.h>
 using namespace std;
+
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
  
-// A utility function to find maximum of three integers
 int max(int a, int b, int c) {
     return max(max(a, b), c);
 }
@@ -36,9 +34,9 @@ int maxcs(int arr[], int l, int m, int h)
 int maxsubarraysum(int arr[], int l, int h)
 {
     if (l > h)
-        return INT_MIN; // invalid condition
+        return INT_MIN; 
     if (l == h)
-        return arr[l]; // base condition
+        return arr[l]; /
     int m = (l + h) / 2;
     return max(maxsubarraysum(arr, l, m - 1), maxsubarraysum(arr, m + 1, h), maxcs(arr, l, m, h));
 }
@@ -53,6 +51,6 @@ int main()
         cout << "Enter " << i << " element : ";
         cin >> arr[i];
     }
-    cout << "The maximum contigous sum is : " << maxsubarraysum(arr, 0, N - 1);
+    cout << "The maximum continous sum is : " << maxsubarraysum(arr, 0, N - 1);
     return 0;
 }
